117e0fbd524770445574c6c9cedd80d96421d8d8
Alexandre Cousin <alexandre.cousin@citae.fr>
2015-06-26 18:33:40 +0200

    commit

--- app/src/main/java/fr/kazutoshi/serviceslist/MainActivity.java
+++ app/src/main/java/fr/kazutoshi/serviceslist/MainActivity.java
@@ -7,13 +7,17 @@ import android.content.pm.PackageManager;
 import android.content.pm.ServiceInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.List;
@@ -25,13 +29,70 @@ public class MainActivity extends ActionBarActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+
+    EditText editText = (EditText) findViewById(R.id.editText);
+    editText.addTextChangedListener(new TextWatcher() {
+      @Override
+      public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+      }
+
+      @Override
+      public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+      }
+
+      @Override
+      public void afterTextChanged(Editable s) {
+        list(s.toString());
+      }
+    });
+
+    list("");
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+
+
+  private void list(String str) {
+
+    final ActivityManager activityManager =
+            (ActivityManager)getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
 
     LinearLayout layout = (LinearLayout) findViewById(R.id.layout);
 
-    List<PackageInfo> pkgs = getPackageManager().getInstalledPackages(PackageManager.GET_SERVICES);
+    layout.removeAllViews();
 
-    final ActivityManager activityManager =
-        (ActivityManager)getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
+    ProgressBar pb = new ProgressBar(this);
+    layout.addView(pb);
+
+    List<PackageInfo> pkgs = getPackageManager().getInstalledPackages(PackageManager.GET_SERVICES);
 
     for (PackageInfo pkg : pkgs) {
       /*if (pkg.services != null)
@@ -57,60 +118,33 @@ public class MainActivity extends ActionBarActivity {
       }*/
       final String packageName = pkg.packageName;
 
-      LinearLayout horizontalLayout = new LinearLayout(this);
-      horizontalLayout.setOrientation(LinearLayout.HORIZONTAL);
-      Button killButton = new Button(this);
-      TextView textView = new TextView(this);
-      ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(
-          ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
-      textView.setGravity(Gravity.CENTER_VERTICAL);
-      killButton.setLayoutParams(layoutParams);
-      textView.setLayoutParams(layoutParams);
-      killButton.setText("X");
-      textView.setText(pkg.packageName);
+      if (packageName.contains(str)) {
 
-      killButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-          activityManager.killBackgroundProcesses(packageName);
-        }
-      });
+        LinearLayout horizontalLayout = new LinearLayout(this);
+        horizontalLayout.setOrientation(LinearLayout.HORIZONTAL);
+        Button killButton = new Button(this);
+        TextView textView = new TextView(this);
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        textView.setGravity(Gravity.CENTER_VERTICAL);
+        killButton.setLayoutParams(layoutParams);
+        textView.setLayoutParams(layoutParams);
+        killButton.setText("X");
+        textView.setText(pkg.packageName);
 
-      horizontalLayout.addView(killButton);
-      horizontalLayout.addView(textView);
+        killButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            activityManager.killBackgroundProcesses(packageName);
+          }
+        });
 
-      layout.addView(horizontalLayout);
+        horizontalLayout.addView(killButton);
+        horizontalLayout.addView(textView);
+
+        layout.addView(horizontalLayout);
+      }
     }
-    layout.removeView(findViewById(R.id.progressBar));
-  }
-
-  @Override
-  protected void onStart() {
-    super.onStart();
-
-
-  }
-
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
+    layout.removeView(pb);
   }
 }
